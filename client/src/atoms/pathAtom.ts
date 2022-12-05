36bc0bbc060cc40f99535f197f6c6008eedc0a31
hanjaegyeong <hjg000223@naver.com>
2022-12-06 04:52:34 +0900

    new project

--- client/src/atoms/pathAtom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const pathState = atom({
-  key: "pathState",
-  default: "home",
-});
--- client/src/atoms/todayAtom.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-import moment from "moment";
-
-export const todayState = atom({
-  key: "todayState",
-  default: moment(),
-});
